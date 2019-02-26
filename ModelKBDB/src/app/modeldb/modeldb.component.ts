620f455df2b35180b3bd65e2c6adb234881f2361
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-02-25 21:14:42 -0600

    ModelDB Component

---
+++ ModelKBDB/src/app/modeldb/modeldb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modeldb',
+  templateUrl: './modeldb.component.html',
+  styleUrls: ['./modeldb.component.css']
+})
+export class ModeldbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
