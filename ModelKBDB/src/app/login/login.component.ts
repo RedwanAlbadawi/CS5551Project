5b3d01397040046097210919ebc99ddcf54f5d01
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-03-04 14:01:59 -0600

    Basics of login authentication done

--- ModelKBDB/src/app/login/login.component.ts
+++ ModelKBDB/src/app/login/login.component.ts
@@ -8,12 +8,32 @@ import {AngularFireAuth} from 'angularfire2/auth';
 })
 export class LoginComponent implements OnInit {
 
+  private email: string;
+  private password: string;
+  alert = '';
+
   constructor(private firebaseauth: AngularFireAuth) {
 
   }
 
   loginEvent(value: any) {
-    console.log('works');
+    this.email = value.email.toString();
+    this.password = value.password.toString();
+    console.log(this.email);
+    console.log(this.password);
+
+    if (this.email !== '' && this.password !== '') {
+      try {
+        this.firebaseauth.auth.signInWithEmailAndPassword(this.email, this.password).then(() => {
+          console.log('logged in');
+        }).catch(() => {
+          this.alert = 'Try again. Invalid Credentials';
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
   }
 
   ngOnInit() {
