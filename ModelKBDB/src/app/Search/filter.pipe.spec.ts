c2c8a0ba94d9402e414c8b196187396ea18001ec
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-03-20 08:42:01 -0500

    Revert to 5db6eae507240e8fc56700f57662f4547c9bbbb1

--- ModelKBDB/src/app/Search/filter.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { addProviders, async, inject} from '@angular/core/testing';
-import { FilterPipe } from './filter.pipe';
-
-describe('Pipe : Filter', () => {
-  it('create an instance', () => {
-    let pipe = new FilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- ModelKBDB/src/app/app.module.ts
+++ ModelKBDB/src/app/app.module.ts
@@ -12,17 +12,8 @@ import { ModeldbComponent } from './modeldb/modeldb.component';
 import { routing } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { FilterPipe } from './Search/filter.pipe';
-=======
 import {AuthService} from './auth/auth.service';
 import {AuthGuard} from './auth/auth.guard';
->>>>>>> 5db6eae507240e8fc56700f57662f4547c9bbbb1
-=======
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth.guard';
->>>>>>> 5db6eae507240e8fc56700f57662f4547c9bbbb1
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -40,8 +31,7 @@ const firebaseConfig = {
     LoginComponent,
     RegisterComponent,
     ModeldbComponent,
-    HeaderComponent,
-    FilterPipe
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
