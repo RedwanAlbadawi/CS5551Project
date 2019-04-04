e49124f3090fc53959c900391f24ea68e548e952
redwan albadawi <raamv7@mail.umkc.edu>
2019-04-04 19:27:48 +0500

    increments 2 updates

---
+++ ModelKBDB/routes/fileUpload.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fileUploadRouter = express.Router();
+const mongoose=require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const crypto = require('crypto');
+
+const mongoURI = 'mongodb+srv://lab7new:lab7new@cluster0-ydpjq.mongodb.net/test?retryWrites=true';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('files');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+fileUploadRouter.post('/upload', upload.single('file'), (req, res) => {
+    console.log(req.body,"file submitted ");
+  });
+
+module.exports= fileUploadRouter;
