8b65165475abf46f556b0842412bc4c0a5f9be74
redwan albadawi <raamv7@mail.umkc.edu>
2019-04-04 19:28:59 +0500

    increments 2 updates

--- ModelKBDB/app.js
+++ ModelKBDB/app.js
@@ -2,13 +2,14 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let logger = require('morgan');
-
+let Experiment = require('./models/experiment');
 let mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://modelkb:modelkb@modelkb-0dva5.gcp.mongodb.net/ModelKB?retryWrites=true')
-  .then(() => console.log('connection successful'))
+//mongoose.connect('mongodb+srv://modelkb:modelkb@modelkb-0dva5.gcp.mongodb.net/ModelKB?retryWrites=true', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://lab7new:lab7new@cluster0-ydpjq.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+  .then(() => console.log( 'connection successful'))
   .catch((err) => console.error(err));
-
 let apiRouter = require('./routes/experiment');
+let apiFileUploadRouter = require('./routes/fileUpload');
 
 let app = express();
 
@@ -19,14 +20,15 @@ app.use(express.static(path.join(__dirname, 'dist/ModelKBDB')));
 app.use('/modeldb/details/:id', express.static(path.join(__dirname, 'dist/ModelKBDB')));
 app.use('/modeldb/upload', express.static(path.join(__dirname, 'dist/ModelKBDB')));
 app.use('/api', apiRouter);
+app.use('/apiFile', apiFileUploadRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
