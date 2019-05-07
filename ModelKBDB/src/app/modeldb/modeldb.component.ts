1da9491615fe7a1a774241f2a7e6e1c619c264a7
redwanalbadawi <raamv7@mail.umkc.edu>
2019-05-08 01:08:34 +0500

    search part version 2

--- ModelKBDB/src/app/modeldb/modeldb.component.ts
+++ ModelKBDB/src/app/modeldb/modeldb.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
 import { ApiService } from '../api.service';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-modeldb',
@@ -10,9 +11,9 @@ import { Observable } from 'rxjs';
 })
 export class ModeldbComponent implements OnInit {
 
-  experiments: any;
+  experiments: [];
   displayedColumns = ['model_name', 'InputTensors', 'OutputTensor', 'Optimizer', 'AccuracyValue'];
-  dataSource = new ExperimentDataSource(this.api);
+  dataSource;
 
 
   constructor(private api: ApiService) { }
@@ -23,23 +24,28 @@ export class ModeldbComponent implements OnInit {
       .subscribe(res => {
         console.log(res);
         this.experiments = res;
+        this.dataSource = new MatTableDataSource(this.experiments);
       }, err => {
         console.log(err);
       });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
 
-export class ExperimentDataSource extends DataSource<any> {
-  constructor(private api: ApiService) {
-    super();
-  }
+// export class ExperimentDataSource extends DataSource<any> {
+//   constructor(private api: ApiService) {
+//     super();
+//   }
 
-  connect() {
-    return this.api.getExperiments();
-  }
+//   connect() {
+//     return this.api.getExperiments();
+//   }
 
-  disconnect() {
+//   disconnect() {
 
-  }
-}
+//   }
+// }
